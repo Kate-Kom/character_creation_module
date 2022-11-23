a7df6cb5a8f07cda0a2fa58e5b5111066985102b
Kate Sharafieva <komrakova-ekaterina93@ya.ru>
2022-11-23 03:25:50 +0300

    Lesson 5/8, theme 8/10, task 2.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Scoring on attack for each class."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон,'
                 f'равный {5 + randint(3, 5)}')
@@ -20,6 +21,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Damage block for each class."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -29,6 +31,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """..."""
     if char_class == 'warrior':
         return (f'{char_name} применил умение «Выносливость {80 + 25} »')
     if char_class == 'mage':
@@ -38,6 +41,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Class description and training."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -62,6 +66,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Character class selection."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
